cd5f381b13878ef1e164fbc1edfab89ddd1de60d
thallyta-castro-cv <thallyta180136319@gmail.com>
2023-11-19 11:39:25 -0300

    feat: criado config para habilitar cors de forma global no projeto.

---
+++ src/main/java/br/com/thallyta/algafood/core/web/WebConfig.java
@@ -0,0 +1,15 @@
+package br.com.thallyta.algafood.core.web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("*");
+    }
+}
