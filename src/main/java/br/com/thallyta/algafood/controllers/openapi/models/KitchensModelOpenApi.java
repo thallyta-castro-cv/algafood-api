679a8eb550c2f92fe6100c4fd48824d4c7bd7e98
thallyta-castro-cv <thallyta180136319@gmail.com>
2024-09-25 13:27:11 -0300

    feat: ajustado paginação no retorno de cozinhas

--- src/main/java/br/com/thallyta/algafood/controllers/openapi/models/KitchensModelOpenApi.java
+++ src/main/java/br/com/thallyta/algafood/controllers/openapi/models/KitchensModelOpenApi.java
@@ -2,10 +2,25 @@ package br.com.thallyta.algafood.controllers.openapi.models;
 
 import br.com.thallyta.algafood.models.dtos.responses.KitchenResponseDTO;
 import io.swagger.annotations.ApiModel;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.hateoas.Links;
+
+import java.util.List;
 
 @ApiModel("KitchensModel")
 @Getter
 @Setter
-public class KitchensModelOpenApi extends PageModelOpenApi<KitchenResponseDTO>{}
+public class KitchensModelOpenApi{
+
+    private KitchensEmbeddedModelOpenApi _embedded;
+    private Links _links;
+    private PagedModelOpenApi page;
+
+    @ApiModel("KitchensEmbeddedModel")
+    @Data
+    public static class KitchensEmbeddedModelOpenApi {
+        private List<KitchenResponseDTO> kitchens;
+    }
+}
--- src/main/java/br/com/thallyta/algafood/core/springfox/SpringFoxConfig.java
+++ src/main/java/br/com/thallyta/algafood/core/springfox/SpringFoxConfig.java
@@ -16,6 +16,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.Links;
+import org.springframework.hateoas.PagedModel;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -54,7 +55,7 @@ public class SpringFoxConfig {
                 .directModelSubstitute(Pageable.class, PageableModelApi.class)
                 .directModelSubstitute(Links.class, LinksModelOpenApi.class)
                 .alternateTypeRules(AlternateTypeRules.newRule(
-                        typeResolver.resolve(Page.class, KitchenResponseDTO.class),
+                        typeResolver.resolve(PagedModel.class, KitchenResponseDTO.class),
                         KitchensModelOpenApi.class))
                 .alternateTypeRules(AlternateTypeRules.newRule(
                         typeResolver.resolve(Page.class, OrderSummaryResponseDTO.class),
